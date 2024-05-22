a71f8188d15c20cf7727edfe7cdd53ba823fe8a2
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-22 10:56:52 -0600

    Added the license list question with choices to choose from

--- index.js
+++ index.js
@@ -8,34 +8,50 @@ const questions = [
         type: "input",
         message: "What is your project's title?",
         name: "title",
+    }, {
 
         type: "input",
         message: "Add the description of your project",
         name: "description",
+    }, {
+
 
         type: "input",
         message: "What are the steps required to install your project?",
         name: "installation",
+    }, {
+
 
         type: "input",
         message: "Provide instructions and examples for use.",
         name: "usage",
+    }, {
+
 
         type: "input",
         message: "List your collaborators or any third party assets",
         name: "contribution",
+    }, {
+
 
         type: "input",
         message: "Test instructions",
-        name: "tests"
+        name: "tests",
+    }, {
+        type: "list",
+        message:"What license will you use?",
+        name: "license",
+        choices: ["MIT License", "Apache 2.0 License", "Boost Software License 1.0", "Mozilla Public License 2.0"]
     }
+
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
