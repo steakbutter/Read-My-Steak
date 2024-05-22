d5c433673340f057d392910e191c0e95a59bf4b4
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-22 10:50:16 -0600

    Added all the input questions to the questions object

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { type } = require('os');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "title",
+
+        type: "input",
+        message: "Add the description of your project",
+        name: "description",
+
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "installation",
+
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage",
+
+        type: "input",
+        message: "List your collaborators or any third party assets",
+        name: "contribution",
+
+        type: "input",
+        message: "Test instructions",
+        name: "tests"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
